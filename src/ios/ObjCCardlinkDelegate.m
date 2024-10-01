9d40376f5d7253957a2afdb28753691ecef09e29
nikito96 <nikinikolitsov9655@gmail.com>
2024-10-01 20:03:24 +0300

    setKeepCallback in iso

--- src/ios/ObjCCardlinkDelegate.m
+++ src/ios/ObjCCardlinkDelegate.m
@@ -69,6 +69,7 @@
     if (cardLinkPluginObject != nil && onErrorCallbackId != nil) {
         NSDictionary* errorResponse = @{ @"message": message };
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:errorResponse];
+        [pluginResult setKeepCallback:YES];
         
         [cardLinkPluginObject.commandDelegate sendPluginResult:pluginResult callbackId:onErrorCallbackId];
     }
