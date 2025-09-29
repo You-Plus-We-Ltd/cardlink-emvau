0d893d0d04f8e2dbc938a97057fe23641951ce5b
Nikola Nikolitsov <nikinikolitsov9655@gmail.com>
2025-09-29 14:46:36 +0300

    remove not used code anymore

--- src/ios/ObjCCardlinkDelegate.m
+++ src/ios/ObjCCardlinkDelegate.m
@@ -6,49 +6,11 @@ static NSTimer* timer = nil;
 
 @implementation ObjCCardlinkDelegate
 
-// - (void) doResult:(NSTimer *)timer
-// {
-//     NSDictionary* userInfo = timer.userInfo;
-//     NSString* stateName = userInfo[@"stateName"];
-//     CardLinkPlugin* cardLinkPluginObject = [CardLinkPlugin cardLinkPluginObject];
-//     NSString* onStateChangedCallbackId = [CardLinkPlugin onStateChangedCallbackId];
-//     CDVPluginResult* pluginResult = nil;
-//     NSMutableArray* passedIds = [CardLinkPlugin passedIds];
-    
-//     if (passedIds == nil) {
-//         passedIds = [[NSMutableArray alloc] init];
-//     }
-    
-//     NSLog(@"TEST-DEBUG: State changed %@ callbackId: %@ passedIds length %lu", stateName, onStateChangedCallbackId, [passedIds count]);
-    
-//     if (![passedIds containsObject:onStateChangedCallbackId]) {
-//         NSLog(@"EST-DEBUG inside %@", onStateChangedCallbackId);
-//         [passedIds addObject:onStateChangedCallbackId];
-//         if (cardLinkPluginObject != nil && onStateChangedCallbackId != nil) {
-//             NSDictionary* stateResponse = @{ @"state": stateName, @"callbackId": onStateChangedCallbackId };
-//             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:stateResponse];
-//             [cardLinkPluginObject.commandDelegate sendPluginResult:pluginResult callbackId:onStateChangedCallbackId];
-//         }
-//         [CardLinkPlugin setPassedIds: passedIds];
-//         [timer invalidate];
-//         timer = nil;
-//     }
-// }
-
 - (void)cardlinkDidChangeState:(CSDKCardlinkState *)state
 {
    CardLinkPlugin* cardLinkPluginObject = [CardLinkPlugin cardLinkPluginObject];
    NSString* onStateChangedCallbackId = [CardLinkPlugin onStateChangedCallbackId];
    CDVPluginResult* pluginResult = nil;
-    
-//    NSLog(@"TEST-DEBUG: State changed %@ callbackId: %@", [state name], onStateChangedCallbackId);
-    
-    // timer = [NSTimer scheduledTimerWithTimeInterval:0.100
-    //                                          target:self
-    //                                        selector:@selector(doResult:)
-
-    //                                        userInfo:@{@"stateName": [state name]}
-    //                                         repeats:YES];
     
    if (cardLinkPluginObject != nil && onStateChangedCallbackId != nil) {
        NSDictionary* stateResponse = @{ @"state": [state name], @"callbackId": onStateChangedCallbackId };
